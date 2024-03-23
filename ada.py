faad024f38f13b56d0ed4dc2fb74ccb030af424f
Quimey5 <162723087+Quimey5@users.noreply.github.com>
2024-03-23 19:29:47 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+    full_name="AdA" +" " + "LoVeLAce" 
+    print(full_name.lower())
+    print(full_name.title())
+    print(full_name.upper())
+    print("\t" + full_name.lower())
