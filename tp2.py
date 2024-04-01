d5d427df8d857b6f993b193511923415846a0ebd
Quimey5 <162723087+Quimey5@users.noreply.github.com>
2024-04-01 18:34:36 -0300

    Add files via upload

---
+++ tp2.py
@@ -0,0 +1,27 @@
+#Ejercicio1
+first_name="AdA"
+last_name="LoVeLAce"
+full_name="AdA" + "LoVeLAce" 
+print(full_name.lower())
+print(full_name.title())
+print(full_name.upper())
+print("\t" + full_name.lower())
+#ejercicio 2
+X= "bangladesh"
+Y= "barbados"
+print(f"te result of {X} come first in the dictionary than {Y} is {X<Y}.")
+print(f"The result of {Y} comes first in the dictionary than {X} is {Y<X} ")
+#ejercicio 3
+expense = 23.75
+money = 100
+Pesos= 76
+Centavos= 25
+print("ingresar gastos:",expense)
+print("Dinero recibido:",money)
+print()
+print("vuelto:")
+print()
+print("Pesos:")
+print(int(money-expense))
+print("Centavos:")
+print((money-expense) - int(money-expense))
